d60c6b40111eeec6c67ad722174825dded6bf6ff
FirstChoice112 <johan.skoog.1986@gmail.com>
2024-05-31 10:27:45 +0200

    Endpoint för att skapa en user är fixat och variabler lagt i Postman

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const connectDB = require("./config/db");
 require("dotenv").config();
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
 //* Connect Database
@@ -9,6 +11,9 @@ connectDB();
 
 const PORT = process.env.PORT || 8000;
 const URL = process.env.BASE_URL || "127.0.0.1";
+
+app.use(express.json());
+app.use("/", authRoutes);
 
 app.listen(PORT, URL, () => {
   console.log(`Listening to http://${URL}:${PORT}`);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,1 +1,55 @@
-//* Innehåller routes för registrering och inloggning.
+//* includes Routes for Auth and login.
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  try {
+    const existingUser = await User.findOne({ email: req.body.email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists 😶‍🌫️" });
+    }
+
+    const newuser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: await bcrypt.hash(req.body.password, 10),
+    });
+    await newuser.save();
+    res.status(201).json({ message: "User created 😀" });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong 😶‍🌫️" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(400).json({ message: "User does not exist 😶‍🌫️" });
+    }
+
+    //* Checks if passwords match
+    const passwordMatch = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!passwordMatch) {
+      return res.status(401).json({ message: "Wrong password" });
+    }
+
+    //* Create JWT-token
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    //* Send token
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong 😶‍🌫️" });
+  }
+});
+
+module.exports = router;
