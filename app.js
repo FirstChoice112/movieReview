f12a6c3f3b855f40ded2ae741eaa3efb48d7e23f
FirstChoice112 <johan.skoog.1986@gmail.com>
2024-05-31 16:49:36 +0200

    Endpointen Addmovie fungerar nu och har lyft authification jwt till en egen middleware fil

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const connectDB = require("./config/db");
 require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
+const movieRoutes = require("./routes/movieRoutes");
 
 const app = express();
 
@@ -14,6 +15,7 @@ const URL = process.env.BASE_URL || "127.0.0.1";
 
 app.use(express.json());
 app.use("/", authRoutes);
+app.use("/movies", movieRoutes);
 
 app.listen(PORT, URL, () => {
   console.log(`Listening to http://${URL}:${PORT}`);
--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,1 +1,21 @@
-//*Hanterar CRUD-operationer för filmer.
+//* Handle movie requests.
+
+//* Add a movie.
+const Movie = require("../models/Movie");
+
+const addMovie = async (req, res) => {
+  try {
+    const { title, director, releaseYear, genre } = req.body;
+
+    const newMovie = new Movie({ title, director, releaseYear, genre });
+    await newMovie.save();
+
+    res.status(201).json(newMovie);
+  } catch (err) {
+    res.status(500).json({ message: "Something went wrong 🤯" });
+  }
+};
+
+module.exports = {
+  addMovie,
+};
