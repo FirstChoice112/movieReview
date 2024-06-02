6840b00ff48071529d3c7fc36866e0d74fad85cd
FirstChoice112 <johan.skoog.1986@gmail.com>
2024-06-02 14:23:48 +0200

    Endpointen Get Movie By Id är tillagd och fungerar felfritt

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -16,6 +16,7 @@ const addMovie = async (req, res) => {
   }
 };
 
+//* Get all movies.
 const getAllMovies = async (req, res) => {
   try {
     const movies = await Movie.find();
@@ -25,7 +26,21 @@ const getAllMovies = async (req, res) => {
   }
 };
 
+//* Get movie by id.
+const getMovieById = async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.id);
+    if (!movie) {
+      return res.status(404).json({ message: "Movie not found 🤷‍♂️" });
+    }
+    res.status(200).json(movie);
+  } catch (err) {
+    res.status(500).json({ message: "Something went wrong 🤯" });
+  }
+};
+
 module.exports = {
   addMovie,
   getAllMovies,
+  getMovieById,
 };
--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,10 +1,15 @@
 //* Movie-routes.
 const express = require("express");
 const router = express.Router();
-const { addMovie, getAllMovies } = require("../controllers/movieController");
+const {
+  addMovie,
+  getAllMovies,
+  getMovieById,
+} = require("../controllers/movieController");
 const authMiddleware = require("../middlewares/authMiddleware");
 
 router.post("/", authMiddleware, addMovie);
 router.get("/", getAllMovies);
+router.get("/:id", getMovieById);
 
 module.exports = router;
