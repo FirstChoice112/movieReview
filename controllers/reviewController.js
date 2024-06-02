890b951dc6facb97f2da42a55ff03322c395a299
FirstChoice112 <johan.skoog.1986@gmail.com>
2024-06-02 19:47:15 +0200

    Get Review By Id tillagt och testad

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -23,7 +23,21 @@ const getAllReviews = async (req, res) => {
   }
 };
 
+//* Get review by id
+const getReviewById = async (req, res) => {
+  try {
+    const review = await Review.findById(req.params.id);
+    if (!review) {
+      return res.status(404).json({ message: "Review not found 🤷‍♂️" });
+    }
+    res.status(200).json(review);
+  } catch (err) {
+    res.status(500).json({ message: "Something went wrong 🤯" });
+  }
+};
+
 module.exports = {
   addReview,
   getAllReviews,
+  getReviewById,
 };
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,9 +1,14 @@
 //* Innehåller routes för recensioner.
 const express = require("express");
 const router = express.Router();
-const { addReview, getAllReviews } = require("../controllers/reviewController");
+const {
+  addReview,
+  getAllReviews,
+  getReviewById,
+} = require("../controllers/reviewController");
 
 router.post("/", addReview);
 router.get("/", getAllReviews);
+router.get("/:id", getReviewById);
 
 module.exports = router;
